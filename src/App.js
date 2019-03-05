b13198ede8eea8f9b5f0d2c69556e52f3196104f
Terry Lee <2tk.java@gmail.com>
2019-03-05 15:56:55 +0900

    loading states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Movie';
+import Movie from './Movie';
 
+const movieTitles = [
+  "Matrix",
+  "Full metal jacket",
+  "Old boy"
+]
+
+const movieImgs = [
+  "https://images-na.ssl-images-amazon.com/images/I/5117ZW5600L._SY445_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/I/71qDKzqJZrL._SL1101_.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+]
 class App extends Component {
+  //componentWillReceiveProps -> shouldComponentUpdate == true -> componentWillUpdate -> render -> componentDidUpdate
+  componentWillMount(){
+    console.log('willMount');
+  }
+  componentDidMount(){
+    console.log('didMount');
+    setTimeout(()=>{
+      this.setState({
+        movies: [
+          {
+            id:1,
+            title:"Matrix",
+            poster:"https://images-na.ssl-images-amazon.com/images/I/5117ZW5600L._SY445_.jpg"
+          },
+          {
+            id:2,
+            title:"Full metal jacket",
+            poster:"https://images-na.ssl-images-amazon.com/images/I/71qDKzqJZrL._SL1101_.jpg"
+          },
+          {
+            id:3,
+            title:"Old boy",
+            poster:"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+          }
+        ]
+      })
+    },5000)
+  }
+  state = {
+  }
+
+  _renderMovies = () => {
+      console.log("_renderMovies");
+      const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    return movies
+  }
   render() {
+    console.log('render');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.movies ? this._renderMovies() : 'Loading'
+        }
       </div>
     );
   }
