649493f6c68f46106f5bdc6f085600d8690d6e4a
Terry Lee <2tk.java@gmail.com>
2019-03-05 21:13:07 +0900

    Updates

---
+++ precache-manifest.fbf6aedd24c71e7c8fbe40cd607a6c19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a264189c0b6275480da",
+    "url": "/reactByNomad/static/css/main.72295c88.chunk.css"
+  },
+  {
+    "revision": "5a264189c0b6275480da",
+    "url": "/reactByNomad/static/js/main.5a264189.chunk.js"
+  },
+  {
+    "revision": "ec99f3fe941460c5e8bc",
+    "url": "/reactByNomad/static/js/runtime~main.ec99f3fe.js"
+  },
+  {
+    "revision": "7e663f1ca65466bd5ee1",
+    "url": "/reactByNomad/static/js/2.7e663f1c.chunk.js"
+  },
+  {
+    "revision": "fee3158b4a90138c1ef2181196843d7e",
+    "url": "/reactByNomad/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.2900952a40a294fe24153a2d56a3934e.js"
+  "/reactByNomad/precache-manifest.fbf6aedd24c71e7c8fbe40cd607a6c19.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/reactByNomad/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
