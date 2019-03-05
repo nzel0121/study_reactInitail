078f4fe336ad46a42242df7db42f8e61990046ab
Terry Lee <2tk.java@gmail.com>
2019-03-05 19:53:14 +0900

    study concept of promise , and refactor code with async / await

--- src/App.js
+++ src/App.js
@@ -20,36 +20,25 @@ class App extends Component {
     console.log('willMount');
   }
   componentDidMount(){
-    console.log('didMount');
-    setTimeout(()=>{
-      this.setState({
-        movies: [
-          {
-            id:1,
-            title:"Matrix",
-            poster:"https://images-na.ssl-images-amazon.com/images/I/5117ZW5600L._SY445_.jpg"
-          },
-          {
-            id:2,
-            title:"Full metal jacket",
-            poster:"https://images-na.ssl-images-amazon.com/images/I/71qDKzqJZrL._SL1101_.jpg"
-          },
-          {
-            id:3,
-            title:"Old boy",
-            poster:"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-          }
-        ]
-      })
-    },5000)
+    this._getMovies();
   }
-  state = {
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies 
+    })
   }
-
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+        .then(potato => potato.json())
+        .then(json => json.data.movies)
+        .catch(err => console.log(err))
+  }
+  state = {}
   _renderMovies = () => {
       console.log("_renderMovies");
-      const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+      const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id}/>
     })
     return movies
   }
